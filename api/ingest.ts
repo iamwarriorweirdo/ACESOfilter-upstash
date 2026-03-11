b9c857fbb51cb77f7aea727ee26949d02818561b
Luckytobealive <tdt19092004@gmail.com>
2026-03-12 05:52:52 +0700

    Delete api/ingest.ts

--- api/ingest.ts
+++
@@ -1,6 +0,0 @@
-
-// This file is deprecated. Logic resides in api/inngest.ts
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-export default function handler(req: VercelRequest, res: VercelResponse) {
-    res.status(410).json({ error: "Endpoint deprecated. Use api/inngest.ts" });
-}
